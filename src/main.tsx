86e6eeb3cb26657dc4215650f26c4195178d8418
Zubkov Igor <zubkovigor@mail.ru>
2024-03-03 21:34:40 +0200

    исправил багу

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import 'normalize.css';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <>
+  <React.StrictMode>
     <App />
-  </>,
+  </React.StrictMode>,
 );
